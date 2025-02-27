c5e27a8c3faf726f568effd46ffbbef52f0cae5c
Nat <laura.rojas-r@mail.escuelaing.edu.co>
2025-02-26 19:49:52 -0500

    tests

--- src/test/java/org/adaschool/project/controller/purchase/PurchaseControllerTest.java
+++ src/test/java/org/adaschool/project/controller/purchase/PurchaseControllerTest.java
@@ -31,16 +31,14 @@ class PurchaseControllerTest {
 
     @Test
     void testGetAllPurchases() {
-        // Arrange
+
         Purchase purchase1 = new Purchase("1", "user1", "product1", 2, 100.0, new Date());
         Purchase purchase2 = new Purchase("2", "user2", "product2", 1, 50.0, new Date());
         List<Purchase> purchases = Arrays.asList(purchase1, purchase2);
         when(purchaseService.getAllPurchases()).thenReturn(purchases);
 
-        // Act
         ResponseEntity<List<Purchase>> response = purchaseController.getAllPurchases();
 
-        // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(purchases, response.getBody());
     }
@@ -48,57 +46,49 @@ class PurchaseControllerTest {
 
     @Test
     void testCreatePurchase() {
-        // Arrange
+
         PurchaseDTO purchaseDTO = new PurchaseDTO("user1", "product1", 2, 100.0, new Date());
         Purchase createdPurchase = new Purchase("1", "user1", "product1", 2, 100.0, new Date());
         when(purchaseService.createPurchase(purchaseDTO)).thenReturn(createdPurchase);
 
-        // Act
         ResponseEntity<Purchase> response = purchaseController.createPurchase(purchaseDTO);
 
-        // Assert
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertEquals(createdPurchase, response.getBody());
     }
 
     @Test
     void testDeletePurchase() {
-        // Arrange
+
         doNothing().when(purchaseService).deletePurchase("1");
 
-        // Act
         ResponseEntity<Void> response = purchaseController.deletePurchase("1");
 
-        // Assert
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 
     @Test
     void testGetShoppingCart() {
-        // Arrange
+
         Map<String, List<String>> shoppingCart = new HashMap<>();
         shoppingCart.put("seller1", Arrays.asList("product1", "product2"));
         when(purchaseService.getProductsById("user1")).thenReturn(shoppingCart);
 
-        // Act
         Map<String, List<String>> response = purchaseController.getShoppingCart("user1");
 
-        // Assert
         assertEquals(shoppingCart, response);
     }
 
     @Test
     void testAddToShoppingCart() {
-        // Arrange
+
         Map<String, String> request = new HashMap<>();
         request.put("idUser", "user1");
         request.put("idProduct", "product1");
         doNothing().when(purchaseService).addProductShoppingCart("user1", "product1");
 
-        // Act
         ResponseEntity<String> response = purchaseController.addToShoppingCart(request);
 
-        // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("product1", response.getBody());
     }
