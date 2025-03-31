1e8fff920b80fe87db957bfc5888d3a921578c52
Ubuntu <IETI@RenovAR.m3cqph3ogoku3fcfyxdjg3yxig.bx.internal.cloudapp.net>
2025-03-31 01:27:24 +0000

    shell

--- exc.sh
+++ exc.sh
@@ -4,4 +4,4 @@ sudo docker stop purchases
 git pull
 sudo docker rm purchases
 sudo docker build -t purchases .
-sudo docker run -d -p 8083:8083 --name purchases purchases
+sudo docker run -d -p 8083:8080 --name purchases purchases
